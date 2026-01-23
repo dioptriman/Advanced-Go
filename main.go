c8a997b6f15a0ba4a9b6aaaf31c11ef2453391e7
Dioptriman <andhikaagung79@gmail.com>
2026-01-23 22:16:12 +0700

    Adding concurrencies stuff and how to handle goroutine problems

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
+	"sync"
 	"time"
 )
 
@@ -70,9 +72,19 @@ func processTruck(truck Truck) error {
 }
 
 func processFleet(trucks []Truck) error {
+	var wg sync.WaitGroup
 	for _, t := range trucks {
-		go processTruck(t)
+		wg.Add(1)
+
+		go func(t Truck) {
+			if err := processTruck(t); err != nil {
+				log.Println(err)
+			}
+			wg.Done()
+		}(t)
 	}
+
+	wg.Wait()
 
 	return nil
 }
